2da706d7b38866bce3a447432f3b751296c9572a
Danish <danish_s@outlook.com>
2023-04-11 18:11:38 +0500

    💻: Setup folder & files

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+#!/usr/bin/env node
+
+console.log(`Danish Saleem
+
+A software engineer, and Open-Source Contributor and a dev tag moderator at Dev.to.I live in Lahore, Pakistan and work as a freelance software engineer. I have skills in JavaScript, ReactJS, NodeJS, Database, Automation, Web Development and Technical Writing. I am also interested in AI, web design, and social media marketing.
+
+`);
